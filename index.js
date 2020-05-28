3a44d80f0bb20b61c6cbacec08e1c1bccc2126e3
AntonioRecaldeRusso <antonio@deckardtech.com>
2020-05-27 23:21:11 -0700

    use es5 declarations

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 module.exports = function s3ParseUrl(url) {
-  const decodedUrl = decodeURIComponent(url);
+  var _decodedUrl = decodeURIComponent(url);
 
-  let result = null;
+  var _result = null;
 
   // http://s3.amazonaws.com/bucket/key1/key2
-  let match = decodedUrl.match(/^https?:\/\/s3.amazonaws.com\/([^\/]+)\/?(.*?)$/);
-  if (match) {
-    result = {
-      bucket: match[1],
-      key: match[2],
+  var _match = _decodedUrl.match(/^https?:\/\/s3.amazonaws.com\/([^\/]+)\/?(.*?)$/);
+  if (_match) {
+    _result = {
+      bucket: _match[1],
+      key: _match[2],
       region: '',
     };
   }
 
   // http://s3-aws-region.amazonaws.com/bucket/key1/key2
-  match = decodedUrl.match(/^https?:\/\/s3-([^.]+).amazonaws.com\/([^\/]+)\/?(.*?)$/);
-  if (match) {
-    result = {
-      bucket: match[2],
-      key: match[3],
-      region: match[1],
+  _match = _decodedUrl.match(/^https?:\/\/s3-([^.]+).amazonaws.com\/([^\/]+)\/?(.*?)$/);
+  if (_match) {
+    _result = {
+      bucket: _match[2],
+      key: _match[3],
+      region: _match[1],
     };
   }
 
   // http://bucket.s3.amazonaws.com/key1/key2
-  match = decodedUrl.match(/^https?:\/\/([^.]+).s3.amazonaws.com\/?(.*?)$/);
-  if (match) {
-    result = {
-      bucket: match[1],
-      key: match[2],
+  _match = _decodedUrl.match(/^https?:\/\/([^.]+).s3.amazonaws.com\/?(.*?)$/);
+  if (_match) {
+    _result = {
+      bucket: _match[1],
+      key: _match[2],
       region: '',
     };
   }
 
   // http://bucket.s3-aws-region.amazonaws.com/key1/key2 or,
   // http://bucket.s3.aws-region.amazonaws.com/key1/key2
-  match = decodedUrl.match(/^https?:\/\/([^.]+).(?:s3-|s3\.)([^.]+).amazonaws.com\/?(.*?)$/);
-  if (match) {
-    result = {
-      bucket: match[1],
-      key: match[3],
-      region: match[2],
+  _match = _decodedUrl.match(/^https?:\/\/([^.]+).(?:s3-|s3\.)([^.]+).amazonaws.com\/?(.*?)$/);
+  if (_match) {
+    _result = {
+      bucket: _match[1],
+      key: _match[3],
+      region: _match[2],
     };
   }
 
-  return result;
+  return _result;
 }
